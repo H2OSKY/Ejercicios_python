f84c376777e881d1779c4e778dfe25d6955b7d65
Hoover Becerra <hoover.hernandez@escueladidactica.com>
2023-11-26 16:37:22 -0500

    Agregar nuevos

--- EstructurasDatosPython_3/Ej7.py
+++ EstructurasDatosPython_3/Ej7.py
@@ -3,3 +3,16 @@
 # una lista de compras, elemento por elemento, y 
 # luego imprima la lista completa al final.
 
+lista_compra = []
+
+while True:
+    elemento = input("Ingrese un elemento de la lista de compras (o 'fin' para terminar): ")
+    
+    if elemento.lower() == 'fin':
+        break
+
+    lista_compra.append(elemento)
+
+print("\nLista de Compras:")
+for item in lista_compra:
+    print(item)
--- EstructurasDatosPython_3/Ej8.py
+++ EstructurasDatosPython_3/Ej8.py
@@ -2,3 +2,15 @@
 # Escribe un programa que tome una lista de 
 # números ingresada por el usuario y elimine 
 # todos los números duplicados.
+
+lista_numeros = input("Ingrese una lista de números separados por espacios: ")
+
+# Convertir la entrada del usuario a una lista de números
+numeros = [int(x) for x in lista_numeros.split()]
+
+# Eliminar duplicados utilizando un conjunto (set)
+lista_sin_duplicados = list(set(numeros))
+
+# Mostrar la lista original y la lista sin duplicados
+print("\nLista original:", numeros)
+print("Lista sin duplicados:", lista_sin_duplicados)
